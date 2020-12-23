3ed2630144253e45fee69e6ebcf57671ad56df6b
hello01szy <2856039370@qq.com>
2020-12-23 23:37:07 +0800

    创建cookie.js并导入

--- shareblog/src/lib/cookie.js
+++ shareblog/src/lib/cookie.js
@@ -1,27 +1,27 @@
 // 在cookie中设置对应的key
-function setCookie(name, value, day = 1){
-  var expire = new Date();
-  expire.setTime(expire.getTime() + day * 24 * 60 * 60 * 1000);
-  document.cookie = name + "=" + value + ";expires=" + expire.toGMTString();
+function setCookie (name, value, day = 1) {
+  var expire = new Date()
+  expire.setTime(expire.getTime() + day * 24 * 60 * 60 * 1000)
+  document.cookie = name + '=' + value + ';expires=' + expire.toGMTString()
 }
 
 // 从cookie中取出对应的key
-function getCookie(name){
-  let arr = document.cookie.match(new RegExp("(^| )"+name+"=([^;]*)(;|$)"));
-  if(arr != null){
+function getCookie (name) {
+  const arr = document.cookie.match(new RegExp('(^| )' + name + '=([^;]*)(;|$)'))
+  if (arr != null) {
     return unescape(arr[2])
-  }else{
+  } else {
     return null
   }
 }
 
 // 删除cookie中对应的key
-function clearCookie(name){
-  var expire = new Date();
-  expire.setTime(expire.getTime() - 1);
-  var cval = getCookie(name);
-  if(cval != null){
-    document.cookie= name + "=" + cval + ";expires=" + expire.toGMTString();
+function clearCookie (name) {
+  var expire = new Date()
+  expire.setTime(expire.getTime() - 1)
+  var cval = getCookie(name)
+  if (cval != null) {
+    document.cookie = name + '=' + cval + ';expires=' + expire.toGMTString()
   }
 }
 
--- shareblog/src/main.js
+++ shareblog/src/main.js
@@ -4,7 +4,6 @@ import router from './router'
 import store from './store'
 import '@/assets/css/common.css'
 import 'font-awesome/css/font-awesome.min.css'
-import '@/lib/cookie'
 import cookie from './lib/cookie'
 Vue.config.productionTip = false
 Vue.prototype.cookie = cookie
